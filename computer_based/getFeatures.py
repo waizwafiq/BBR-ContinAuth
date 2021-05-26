405dbc089a4043c1837a0c68725220d74c8e4cf6
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-05-26 22:42:01 +0800

    Created class for obtaining the feats

--- computer_based/getFeatures.py
+++ computer_based/getFeatures.py
@@ -2,12 +2,14 @@ import pyautogui as pag
 import pandas as pd
 import time
 import ctypes
-user32 = ctypes.windll.user32
-screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
-print(screensize)
 
-while(True):
-    pos = pag.position() #get the current position of the mouse
-    x_coord, y_coord = pos[0], screensize[1]-pos[1]
-    print(x_coord, y_coord)
-    time.sleep(0.01)
+def run():
+    user32 = ctypes.windll.user32
+    screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+    print(screensize)
+
+    while(True):
+        pos = pag.position() #get the current position of the mouse
+        x_coord, y_coord = pos[0], screensize[1]-pos[1]
+        print(x_coord, y_coord)
+        time.sleep(0.01)
