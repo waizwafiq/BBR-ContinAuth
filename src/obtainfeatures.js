635a1fdce8b891a6c32352b0531dbd4e29c1aa9b
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-05-26 15:14:34 +0800

    Create obtainfeatures.js

---
+++ src/obtainfeatures.js
@@ -0,0 +1,24 @@
+function downloadCSV(rows) {
+    //To download the CSV file of features
+    let csvContent = "data:text/csv;charset=utf-8," + rows.map(e => e.join(",")).join("\n");
+
+    var encodedUri = encodeURI(csvContent);
+    window.open(encodedUri);
+}
+
+let sampleEle = document.querySelector(".sample");
+
+let features = []; //C: mouse coordinates
+document.body.addEventListener("mousemove", (event) => {
+    let instantFeats = []; //the instantaneous features arraylist
+
+    /*PUT FEATURES TO BE OBTAINED HERE*/
+    instantFeats.push(event.x); //get x coordinate
+    instantFeats.push(event.y); //get y coordinate
+
+
+    features.push(instantFeats); 
+    // sampleEle.innerHTML = "X axis: " + event.x + " Y axis: " + event.y;
+});
+
+document.getElementById("dwCSV").onclick = function() {downloadCSV(features)}; //download features when the button is clicked
