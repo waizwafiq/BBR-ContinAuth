9ebdcc2270c085cc608c3674c52aea22123b4b82
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-05-26 23:49:24 +0800

    Added dictionary for features

--- computer_based/getFeatures.py
+++ computer_based/getFeatures.py
@@ -3,13 +3,28 @@ import pandas as pd
 import time
 import ctypes
 
-def run():
+
+def run(time_interval: float, stopTime: float):
+    features = {
+        't': [],
+        'x': [],
+        'y': []
+    }
+
     user32 = ctypes.windll.user32
     screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
     print(screensize)
+    t = 0
+    while t // 1 < stopTime:
+        pos = pag.position()  # get the current position of the mouse
+        x_coord, y_coord = pos[0], screensize[1] - pos[1]
+        t += time_interval
+        # print(t, x_coord, y_coord)
 
-    while(True):
-        pos = pag.position() #get the current position of the mouse
-        x_coord, y_coord = pos[0], screensize[1]-pos[1]
-        print(x_coord, y_coord)
-        time.sleep(0.01)
+        features['t'].append(t)
+        features['x'].append(x_coord)
+        features['y'].append(y_coord)
+
+        time.sleep(time_interval)
+
+    return features
