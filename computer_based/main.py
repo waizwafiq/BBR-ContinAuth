52cde5763e9e619e882d1ecab447e9bb6bf28598
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-05-26 23:48:58 +0800

    Improved main.py for testing purposes

--- computer_based/main.py
+++ computer_based/main.py
@@ -1,4 +1,8 @@
 import login, getFeatures
+import pandas as pd
 
-login.login.run()
-getFeatures.run()
+#login.login.run()
+features = getFeatures.run(0.01, 10) #record for 10 seconds with 0.01s interval
+
+feat_df = pd.DataFrame(features)
+feat_df.to_csv('test.csv') #save data into CSV
