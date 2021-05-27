dbff92830ee0af39aa945f151c69d22d9c9ea2c1
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-05-27 14:25:40 +0800

    Made minor changes

--- computer_based/main.py
+++ computer_based/main.py
@@ -1,8 +1,10 @@
 import login, getFeatures
 import pandas as pd
 
-#login.login.run()
-features = getFeatures.run(0.01, 10) #record for 10 seconds with 0.01s interval
+# login.login.run()
+features = getFeatures.run(0.01, 3)  # record for 10 seconds with 0.01s interval
 
 feat_df = pd.DataFrame(features)
-feat_df.to_csv('test.csv') #save data into CSV
+print(feat_df)
+
+feat_df.to_csv('test.csv')  # save data into CSV
