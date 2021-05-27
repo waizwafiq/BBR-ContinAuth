e6ae533724d101a1116ac3a8b5e511c2138dbee2
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-05-27 16:09:38 +0800

    Improved data acquisition.
    Data will not be recorded when the mouse is not moving to avoid outliers and reduce space consumption.

--- computer_based/getFeatures.py
+++ computer_based/getFeatures.py
@@ -25,7 +25,7 @@ def run(time_interval: float, stopTime: float):
     pos = pag.position()
     x_i, y_i = pos[0], screensize[1] - pos[1]  # get initial coordinates
 
-    t, t_p = 0, time_interval
+    t, t_p = 0 - time_interval, time_interval  # (0 - time_interval) to get the initial data features (t=0)
     sum_se = 0  # cumulative s_euclid and s_manhattan
     while (t // 1) < stopTime:
         '''TIME'''
@@ -38,26 +38,29 @@ def run(time_interval: float, stopTime: float):
         '''POSITIONS/SPATIAL FEATURES'''
         pos = pag.position()  # get the current position of the mouse
         x_f, y_f = pos[0], screensize[1] - pos[1]
-        dx, dy = (x_f - x_i), (y_f - y_i)  # get differentials
-        x_i, y_i = x_f, y_f  # update the initial coordinates with current
-        s_e = (dx ** 2 + dy ** 2) ** 0.5
-        sum_se += s_e
 
-        '''TEMPORAL FEATURES'''
-        v_x, v_y = dx / dt, dy / dt  # calculate horizontal and vertical velocity
-        v_t = (v_x ** 2 + v_y ** 2) ** 0.5
+        if x_f != x_i or y_f != y_i or t == 0:
+            # obtain features when the mouse is moving only
+            dx, dy = (x_f - x_i), (y_f - y_i)  # get differentials
+            x_i, y_i = x_f, y_f  # update the initial coordinates with current
+            s_e = (dx ** 2 + dy ** 2) ** 0.5
+            sum_se += s_e
 
-        '''ADDING INTO FEATURES DICTIONARY'''
-        features['t'].append(t)
-        features['x'].append(x_f)
-        features['y'].append(y_f)
-        features['dx'].append(dx)
-        features['dy'].append(dy)
-        features['s_euclid'].append(s_e)
-        features['s_euclid_c'].append(sum_se)
-        features['v_x'].append(v_x)
-        features['v_y'].append(v_y)
-        features['v'].append(v_t)
+            '''TEMPORAL FEATURES'''
+            v_x, v_y = dx / dt, dy / dt  # calculate horizontal and vertical velocity
+            v_t = (v_x ** 2 + v_y ** 2) ** 0.5
+
+            '''ADDING INTO FEATURES DICTIONARY'''
+            features['t'].append(t)
+            features['x'].append(x_f)
+            features['y'].append(y_f)
+            features['dx'].append(dx)
+            features['dy'].append(dy)
+            features['s_euclid'].append(s_e)
+            features['s_euclid_c'].append(sum_se)
+            features['v_x'].append(v_x)
+            features['v_y'].append(v_y)
+            features['v'].append(v_t)
         t_p = timeit.default_timer() - start_tp  # end processing time
 
     return features
