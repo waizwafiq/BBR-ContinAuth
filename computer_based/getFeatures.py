93ac4cb2c9b0a18afd5fbfcabb92ea75e99183c5
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-05-27 14:25:33 +0800

    Added velocity features

--- computer_based/getFeatures.py
+++ computer_based/getFeatures.py
@@ -1,30 +1,49 @@
 import pyautogui as pag
 import pandas as pd
-import time
+import time, timeit
 import ctypes
+
+features = {
+    't': [],
+    'x': [],
+    'y': [],
+    'dx': [],
+    'dy': [],
+    'v_x': [],
+    'v_y': []
+}
 
 
 def run(time_interval: float, stopTime: float):
-    features = {
-        't': [],
-        'x': [],
-        'y': []
-    }
-
     user32 = ctypes.windll.user32
     screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
     print(screensize)
-    t = 0
-    while t // 1 < stopTime:
+
+    pos = pag.position()
+    x_i, y_i = pos[0], screensize[1] - pos[1]  # get initial coordinates
+
+    t, t_p = 0, time_interval
+    while (t // 1) < stopTime:
+        t_i = t
+
+        start_tp = timeit.default_timer()  # start processing time, t_p(0)
+        time.sleep(time_interval)  # pause for (time_interval) seconds
+        t += t_p  # t_f = t_i + t_p
+
         pos = pag.position()  # get the current position of the mouse
-        x_coord, y_coord = pos[0], screensize[1] - pos[1]
-        t += time_interval
-        # print(t, x_coord, y_coord)
+        x_f, y_f = pos[0], screensize[1] - pos[1]
+        dx, dy, dt = (x_f - x_i), (y_f - y_i), (t - t_i)  # get differentials
+        x_i, y_i = x_f, y_f  # update the initial coordinates with current
+
+        v_x, v_y = dx / dt, dy / dt  # calculate horizontal and vertical velocity
 
         features['t'].append(t)
-        features['x'].append(x_coord)
-        features['y'].append(y_coord)
-
-        time.sleep(time_interval)
+        features['x'].append(x_f)
+        features['y'].append(y_f)
+        features['dx'].append(dx)
+        features['dy'].append(dy)
+        features['v_x'].append(v_x)
+        features['v_y'].append(v_y)
+        t_p = timeit.default_timer() - start_tp  # end processing time
 
     return features
